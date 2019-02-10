ddbf29ef4be290e67b500237e0ae3b46b9c06e06
saadhassaan <karim.khamwani@hotmail.com>
2019-02-11 00:24:16 +0500

    react native elements added

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { SafeAreaView } from 'react-native';
+import { Button } from 'react-native-elements';
 import { Provider } from 'react-redux';
 import AppWithNavigationState from './src/navigation/index';
 import store from './src/store';
@@ -12,6 +13,9 @@ class App extends Component {
       <Provider store={store}>
         <SafeAreaView style={{ flex: 1 }}>
           <AppWithNavigationState />
+          <Button
+            title="Solid Button"
+          />
         </SafeAreaView>
       </Provider>
     );
